33e1d51b622a27f16103bd40f3e4e6fd9e367f62
ist1101566 <caroline.skjoren@tecnico.ulisboa.pt>
2022-10-31 09:07:28 +0100

    shoppingcart completed , first edition

--- pages/shoppingcart.js
+++ pages/shoppingcart.js
@@ -1,7 +1,3 @@
-document.onload = function () {
-    show();
-};
-
 const getElement = id => document.getElementById(id);
 
 //item 1
@@ -14,84 +10,91 @@ const minusBtn2 = getElement("minusBtn2");
 const amount2 = getElement("amount2");
 
 const item1price = getElement("item1price");
+const item2price = getElement("item2price");
 
 const totalPrice = getElement("totalPrice");
 const checkoutPrice = getElement("checkoutPrice");
 
+const checkoutBtn = getElement("checkoutBtn");
+
+var min = 1, max = 6;
+var number=0;
+
 let totalSum = 0;
 let checkoutSum = 0; 
-const number1 = 1, number2 = 1;
-var min = 1, max = 6;
 var itemPrice1 = 499, itemPrice2 = 299;
-
-amount1.innerText = number1;
-amount2.innerText = number2;
-
-const adjustItem = (id, func, numberX) => {
-    let number = 0;
-    let numberPlace = amount1;
-    if(numberX === 1){
-        number = number1
-    }
-    if(numberX === 2){
-        number = number2
-        numberPlace= amount2;
-    }
-    id.onclick = function(){
-        if(func === 1 ){
-            if(number<max){
-                number ++;
-                console.log(number)
-            }
-        }
-        if(func === 2 ){
-            if(number>min){
-                number --;
-                console.log(number)
-            }
-        }
-        numberPlace.innerText = number;
-        if(numberX===1){
-            number1 = number
-        }
-        else if( numberX === 2){
-            number2 = number
-        }
-    }
-
-    updateItemPrice(numberX)
-    updateUI(); 
-}
-
-const updateItemPrice= (itemNumber) => {
-    if(itemNumber===1){
-        item1price.innerText = number1*itemPrice1;
-    }
-    if(itemNumber===2){
-        item1price.innerText = number2*itemPrice2;
-    }
-}
-
 
 
 const plusBtn1Function = () => {
-    adjustItem(plusBtn1, func = 1, 1);
+    plusBtn1.onclick = function(){
+        number = amount1.firstChild.nodeValue;
+        console.log(number)
+        if(number < max){
+            number ++;
+            amount1.innerText = number;
+            item1price.innerText = number*itemPrice1;
+            updateUI();
+
+        }
+    }
+
 }
 const minusBtn1Function = () => {
-    adjustItem(minusBtn1, func = 2, 1);
+    minusBtn1.onclick = function(){
+        number = amount1.firstChild.nodeValue;
+        console.log(number)
+        if(number > min){
+            number --;
+            amount1.innerText = number;
+            item1price.innerText = number*itemPrice1;
+            updateUI();
+
+        }
+    }
 }
+
+
 const plusBtn2Function = () => {
-    adjustItem(plusBtn2, func = 1, 2);
+    plusBtn2.onclick = function(){
+        number = amount2.firstChild.nodeValue;
+        console.log(number)
+        if(number < max){
+            number ++;
+            amount2.innerText = number;
+            item2price.innerText = number*itemPrice2;    
+            updateUI();
+    
+        }
+    }
+
+
 }
 const minusBtn2Function = () => {
-    adjustItem(minusBtn2, func = 2, 2);
-}
-
-
+minusBtn2.onclick = function(){
+        number = amount2.firstChild.nodeValue;
+        console.log(number)
+        if(number > min){
+            number --;
+            amount2.innerText = number;
+            item2price.innerText = number*itemPrice2;   
+            updateUI();
+     
+        }
+        }
+    }
 
 const updateUI = () => {
-    
-    totalPrice.innerText = totalSum;
+    console.log("updatefunction")
+    totalSum = Number(item1price.firstChild.nodeValue) + Number(item2price.firstChild.nodeValue);
+    console.log(totalSum)
+    checkoutSum = totalSum + 59;
+    totalPrice.innerHTML = totalSum;
     checkoutPrice.innerText = checkoutSum;
 }
 
+const checkOut = ()=> {
+    checkoutBtn.onclick = () => {
+               alert("Thank you for your order");
+
+    }
+}
