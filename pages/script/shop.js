547c83616df2eecd9e64801d550f2b5b0e6b4453
Mattias Formo <matti1499@gmail.com>
2022-11-02 20:37:17 +0100

    Testing multiple classes for shopItems

--- pages/script/shop.js
+++ pages/script/shop.js
@@ -3,18 +3,37 @@ const items = JSON.parse(data)
 
 const shopBody = document.getElementById("shopBody");
 
-console.log(items)
+itemArray = []
+
 for (const id in items) {
     console.log(items[id])
     let item = items[id]
     let shopItem = document.createElement('a')
     shopItem.id = item["name"]
-    shopItem.className = "categoryLink"
+    shopItem.className = `categoryLink ${item["category"]}`
     shopItem.href = "#";
     shopItem.innerHTML = `
         <img src="../../images/${item["image_src"]}">
         <div class="shopCategoryDisplay">
             ${item["name"].replaceAll('_', ' ')}
         </div>`;
-    shopBody.prepend(shopItem)
+    itemArray.push(shopItem)
+}
+
+console.log(itemArray)
+
+for (let i = 0; i < itemArray.length; i++) {
+    shopBody.prepend(itemArray[i])
+}
+
+const necklaces = document.getElementsByClassName("necklace")
+
+console.log(necklaces)
+
+for (let i = 0; i < necklaces.length; i++) {
+    necklaces[i].style.visibility = 'hidden';
+}
+
+for (let i = 0; i < necklaces.length; i++) {
+    necklaces[i].style.visibility = 'visible';
 }
